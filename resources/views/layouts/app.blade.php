f2d87949a4977a2b9966897025c9b328cec82c53
Reymel Sardenia <sardeniareymel@gmail.com>
2026-06-16 21:46:04 +0800

    add bootstrap reference

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Online Exam Platform</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container">
+    <div class="container py-4">
         @yield('content')
     </div>
 </body>
